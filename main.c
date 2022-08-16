137645d02455a4ffc209bd1d7f5d4fa15297dc71
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-16 09:41:05 -0700

    Adding main, functions and testing the command lines

--- main.c
+++ main.c
@@ -6,6 +6,28 @@
  *
  */
 
-int main();
+int main(int argc, char **argv)
 {
-	
+	char *line; 
+	int line_max = 1024;
+	FILE *aux_argv;
+
+	error_message(argc, argv);
+	return(0);
+
+	line = malloc(line_max + 1);
+		if (line == NULL)
+		{
+			fprintf(stderr,"Error: malloc failed");
+			exit(EXIT_FAILURE);
+		}
+
+	*aux_argv = error_message(argc, argv);
+	while (fgets(line, line_max + 1, aux_argv) != NULL)
+	{
+		printf("%s", line);
+	}
+
+	free(line);
+	return(0);
+}
