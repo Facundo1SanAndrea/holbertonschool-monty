969e2d7836354c0fc77b6a36c35ba488fad6528e
gasstin <gastonl1994@gmail.com>
2022-08-16 07:51:50 -0700

    adding main cant open a file

--- monty.c
+++ monty.c
@@ -9,10 +9,22 @@
  */
 int main(int argc, char **argv)
 {
+	FILE *archive;
+//	size_t 
+
 	if (argc != 2)
 	{
 		write(2,"USAGE: monty file\n", 18);
 		exit(EXIT_FAILURE);
 	}
+	archive = fopen(argv[1], "r");
+	if (!archive)
+	{
+		write(2,"Error: Can't open file ", 23);
+		write(2, argv[1], strlen(argv[1]));
+		write(2, "\n", 1);
+		exit(EXIT_FAILURE);
+	}
+	
 	return(0);
 }
