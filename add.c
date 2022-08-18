33ab98836881f2d753e7a0521f3304fb049760fa
gasstin <gastonl1994@gmail.com>
2022-08-18 12:34:18 -0700

    fixing add function

--- add.c
+++ add.c
@@ -15,7 +15,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (element < 2)
 	{
-		fprintf(stderr, "L%d: can´t add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		global_variable = "1";
 		return;
 	}
