1eba04802bec1bc9803c2e66a7094a9a3d34f66c
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-18 11:22:15 -0700

    Fixing mian body and push function

--- push.c
+++ push.c
@@ -5,7 +5,23 @@ char *global_variable;
 void push(stack_t **stack, unsigned int linea)
 {
 	stack_t *aux = NULL;
+	int i = 0;
 
+	while (global_variable[i])
+        {
+                if (global_variable[i] == '-' || global_variable[i] == '+')
+                {
+                        i++;
+                        continue;
+                }
+                else if (!(isdigit(global_variable[i])))
+                {
+                fprintf(stderr,"L%d: usage: push integer\n", linea);
+                exit(EXIT_FAILURE);
+                global_variable = "a";
+                }
+                i++;
+        }
 	aux = malloc(sizeof(stack_t));
 	if (!aux)
 		printf("%d usage: push integer.\n", linea);
