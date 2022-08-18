c053c0848f627b07807a060356da9aff1f91d8c9
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-18 11:26:27 -0700

    Fixing mian body and push function

--- my_print.c
+++ my_print.c
@@ -7,12 +7,12 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-	/*stack_t *head = *stack;*/
+	stack_t *head = *stack;
 
-	if (stack == NULL)
+	if (head == NULL)
 	{
 		fprintf(stderr, "L%u: can´t pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*stack)->n);
+	printf("%d\n", (head)->n);
 }
