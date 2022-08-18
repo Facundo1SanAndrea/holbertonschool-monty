30bb51f1ba74c6ca1145f94a7f90fc2c5553e20c
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-18 05:54:58 -0700

    Adding proto to the header

--- monty.h
+++ monty.h
@@ -44,6 +44,10 @@ int get_func(char *token, unsigned int line_number, stack_t **stack);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 /*void prueba(stack_t **stack, unsigned int line_number);*/
 
 extern char *global_variable;
