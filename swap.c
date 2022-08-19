f4377d0c9e84f26a3a712a1b9e9a17be704b2466
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-19 11:06:32 -0700

    fixing add

--- swap.c
+++ swap.c
@@ -7,25 +7,25 @@
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-	stack_t *head, *strlen;
+	stack_t *strlen, *next;
+	int element = 0;
 
-	if (stack == NULL)
+	for (element = 0; strlen->next; element++)
+	       strlen = strlen->next;
+
+	if (element < 2)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		global_variable = "1";
 		return;
 	}
-	else if (!(*stack))
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		global_variable = "1";
-		return;
-	}
-	else
-	{
-		head = *stack, strlen = (*stack)->next;
-		strlen->prev = NULL, head->prev = strlen;
-		head->next = strlen->next, strlen->next = head;
-		*stack = strlen;
-	}
+
+	next = (*stack)->next;
+	next->prev = (*stack)->prev;
+	(*stack)->prev->next = next;
+	(*stack)->prev = next;
+	(*stack)->next = next->next;
+	next->next->prev = *stack;
+	next->next = *stack;
+	*stack = next;
 }
