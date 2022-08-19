6be7a86a662a7c127d57619d4cfc35131b423027
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-19 12:01:54 -0700

    swap funtion

--- swap.c
+++ swap.c
@@ -7,17 +7,36 @@
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-	int tmp, tmp2, element = 0;
+	stack_t *head, *strlen, *prestack;
+	int tmp, tmp2;
 
-	if (element > 2)
+	head = *stack;
+
+	if (head)
 	{
-		tmp = (*stack)->n;
-        	tmp2 = (*stack)->next->n;
-        	(*stack)->n = tmp2;
-        	(*stack)->next->n = tmp;
+		strlen = head->next;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		global_variable = "1";
+		return;
+	}
+	if (strlen)
+	{
+		prestack = strlen->next;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		global_variable = "1";
+		return;
 	}
 
-	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-	global_variable = "1";
-	return;	
+	if (prestack)
+		prestack->prev = head;
+	tmp = (*stack)->n;
+	tmp2 = (*stack)->next->n;
+	(*stack)->n = tmp2;
+	(*stack)->next->n = tmp;
 }
