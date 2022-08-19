0fcbdcd34c2d954df9d33dd0965dcfd84d1649ef
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-19 10:50:22 -0700

    fixing add

--- swap.c
+++ swap.c
@@ -20,7 +20,6 @@ void swap(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		global_variable = "1";
-		free(stack);
 		return;
 	}
 	else
