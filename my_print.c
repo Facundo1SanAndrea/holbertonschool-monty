9362867d44e6b30ac08034faf8ef737d61a77cb4
gasstin <gastonl1994@gmail.com>
2022-08-19 06:43:56 -0700

    fixing pint

--- my_print.c
+++ my_print.c
@@ -13,7 +13,7 @@ void pint(stack_t **stack, unsigned int line_number)
 		printf("%d\n", (*stack)->n);
 	else
 	{
-		fprintf(stderr, "L%u: can´t pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		global_variable = "1";
 		return;
 	}
