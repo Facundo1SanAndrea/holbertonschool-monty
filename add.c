01f5fed93070b575cb75a4541f8ae1cc254040de
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-19 07:27:49 -0700

    fixing swap

--- add.c
+++ add.c
@@ -14,16 +14,15 @@ void add(stack_t **stack, unsigned int line_number)
 	for (element = 0; strlen->next; element++)
 		strlen = strlen->next;
 
-	if (element < 2)
+	if (element > 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		global_variable = "1";
-		return;
+		head = (*stack)->next;
+		(*stack)->next->n += (*stack)->n;
+		free(*stack);
+		(*stack) = head->next;	
 	}
 
-	head = (*stack)->next;
-	(*stack)->next->n += (*stack)->n;
-	free(*stack);
-/*	head->stack = NULL;*/
-	(*stack) = head->next;
+	fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+	global_variable = "1";
+	return;
 }
