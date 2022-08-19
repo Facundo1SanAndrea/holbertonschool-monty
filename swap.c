7e9d3673b536e4e1430c991e35789e1a5a1504d2
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-19 07:19:27 -0700

    fixing swap

--- swap.c
+++ swap.c
@@ -18,9 +18,9 @@ void swap(stack_t **stack, unsigned int line_number)
 	else
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		global_variable = "1";
+		return;
 	}
-
 	if (strlen)
 	{
 		prestack = strlen->next;
@@ -28,7 +28,8 @@ void swap(stack_t **stack, unsigned int line_number)
 	else
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		global_variable = "1";
+		return;
 	}
 
 	if (prestack)
